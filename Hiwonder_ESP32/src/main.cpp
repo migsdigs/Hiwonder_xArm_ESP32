2b67f5e3f4e0268f0c319a5423120c916fc7114a
migsdigs <miguelito2384@gmail.com>
2023-09-08 10:02:25 +0200

    first message

--- Hiwonder_ESP32/src/main.cpp
+++ Hiwonder_ESP32/src/main.cpp
@@ -16,36 +16,36 @@ void setup() {
 }
 
 void loop() {
-// 	int divisor =4;
+	int divisor =4;
 
-// 	for (int x = -9000; x < 9000; x+=1000) {
-// 		long start = millis();
-// 		int angle = x;
-// 		int16_t pos = 0;
-// 		pos = servo.pos_read();
-// //		Serial.printf("\n\nPosition at %d -> %s\n", pos,
-// //				servo.isCommandOk() ? "OK" : "\n\nERR!!\n\n");
+	for (int x = -9000; x < 9000; x+=1000) {
+		long start = millis();
+		int angle = x;
+		int16_t pos = 0;
+		pos = servo.pos_read();
+//		Serial.printf("\n\nPosition at %d -> %s\n", pos,
+//				servo.isCommandOk() ? "OK" : "\n\nERR!!\n\n");
 
-// 		//do {
-// 			servo.move_time(angle, 10*divisor);
-// 		//} while (!servo.isCommandOk());
-// //		Serial.printf("Move to %d -> %s\n", angle,
-// //				servo.isCommandOk() ? "OK" : "\n\nERR!!\n\n");
-// //		Serial.println("Voltage = " + String(servo.vin()));
-// //		Serial.println("Temp = " + String(servo.temp()));
-// //		Serial.println("ID  = " + String(servo.id_read()));
-// //		Serial.println("Motor Mode  = " + String(servo.readIsMotorMode()));
-// 		long took = millis()-start;
-// 		long time = (10*divisor)-took;
-// 		if(time>0)
-// 			delay(time);
-// 		else{
-// 			Serial.println("Real Time broken, took: "+String(took));
-// 		}
-	// }
+		//do {
+			servo.move_time(angle, 10*divisor);
+		//} while (!servo.isCommandOk());
+//		Serial.printf("Move to %d -> %s\n", angle,
+//				servo.isCommandOk() ? "OK" : "\n\nERR!!\n\n");
+//		Serial.println("Voltage = " + String(servo.vin()));
+//		Serial.println("Temp = " + String(servo.temp()));
+//		Serial.println("ID  = " + String(servo.id_read()));
+//		Serial.println("Motor Mode  = " + String(servo.readIsMotorMode()));
+		long took = millis()-start;
+		long time = (10*divisor)-took;
+		if(time>0)
+			delay(time);
+		else{
+			Serial.println("Real Time broken, took: "+String(took));
+		}
+	}
 
-	// servo.move_time(-9000, 3000);
-	// delay(4000);
+	servo.move_time(-9000, 3000);
+	delay(4000);
 }
 
 // // Playing with PWM pin
